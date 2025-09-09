517fc7b14e0136f2ae29fb825cb4ebf9e213d448
67070187 <67070187@kmitl.ac.th>
2025-09-09 21:00:49 +0700

    eiei

---
+++ cell00/ex04/argv.sh
@@ -0,0 +1,10 @@
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+elif [ $# -gt 3 ]; then
+    echo "maximum 3 arguments"
+else
+    for arg in "$@"
+    do
+        echo "$arg"
+    done
+fi
